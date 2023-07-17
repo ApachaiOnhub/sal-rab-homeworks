69a7292712794dfccd62c01f7c66be5bb4e131c3
Kolesnikov Y.A <apachairu@gmail.com>
2023-07-17 23:08:45 +0800

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,12 +39,11 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
         
     for (let i = 0; i < countOfGoods; i += 1) {
-        let titleCount = {title, count};
-        titleCount = {title: goods[i].title, count: goods[i].count};
+        let titleCount = {title: goods[i].title, count: goods[i].count};
         data.goods.push(titleCount);
         }
 
-    data.order.address = data.order.address + "ул. " + address.street + ", дом " address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
     
     data.order.sum = sum;
     data.client = name + " " + phone;
